237b85166a77e22bf140baa93c89f8ee4048db78
moein67 <mehrparvar.moein@gmail.com>
2023-09-16 16:31:36 +0330

    add admins email notification on new order save

---
+++ app/Observers/OrderObserver.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Observers;
+
+use App\Models\Order;
+use App\Models\User;
+use App\Notifications\NewOrder;
+use Illuminate\Support\Facades\Notification;
+
+class OrderObserver
+{
+    /**
+     * Handle the Order "created" event.
+     */
+    public function created(Order $order): void
+    {
+        ///send email to admins with queueable notification
+        $admins = User::admin()->get();
+        Notification::send($admins, new NewOrder($order));
+    }
+
+    /**
+     * Handle the Order "updated" event.
+     */
+    public function updated(Order $order): void
+    {
+        //
+    }
+
+    /**
+     * Handle the Order "deleted" event.
+     */
+    public function deleted(Order $order): void
+    {
+        //
+    }
+
+    /**
+     * Handle the Order "restored" event.
+     */
+    public function restored(Order $order): void
+    {
+        //
+    }
+
+    /**
+     * Handle the Order "force deleted" event.
+     */
+    public function forceDeleted(Order $order): void
+    {
+        //
+    }
+}
